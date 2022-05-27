4b1bc62ef46dea0822ccf7d7b3415993c3c11c08
Gabriel Calistro <gahcalistro@gmail.com>
2022-05-26 21:14:32 -0300

    style: web responsive. feat: slider with SwiperJS + active nav style function.

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+window.addEventListener("scroll", onScroll);
+onScroll();
+
+function onScroll() {
+  activeNavStyle(home);
+  activeNavStyle(services);
+  activeNavStyle(projects);
+  activeNavStyle(about);
+  activeNavStyle(testimonials);
+}
+
+function activeNavStyle(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionBottom = sectionTop + sectionHeight;
+  const sectionId = section.getAttribute("id");
+
+  const passedTop = targetLine >= sectionTop;
+  const passedBottom = targetLine >= sectionBottom;
+
+  const sectionBoundaries = passedTop && !passedBottom;
+
+  const listElement = document.querySelector(
+    `#navigation ul li a[href*=${sectionId}]`
+  );
+
+  listElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    listElement.classList.add("active");
+  }
+}
+
 function openMenu() {
   document.body.classList.add("menu-expanded");
 }
@@ -7,14 +42,21 @@ function closeMenu() {
 }
 
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
+  // Parameters
   direction: "horizontal",
   loop: true,
   slidesPerView: 1,
   spaceBetween: 30,
-  // If we need pagination
+  grabCursor: true,
+  // Pagination
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 1.4,
+      spaceBetween: 40,
+    },
   },
 });
